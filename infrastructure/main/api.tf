a44515544090a12d401a403294e1ed0c97bb8716
Jack Lavelle <your_email@example.com>
2025-09-02 16:17:37 +0100

    api

--- infrastructure/main/api.tf
+++ infrastructure/main/api.tf
@@ -3,4 +3,5 @@ module "api" {
   get_job_function_arn = module.get_job_function.function_arn
   uploader_function_arn = module.uploader_function.function_arn
   uploader_function_name = module.uploader_function.function_name
+  get_job_function_name = module.get_job_function.function_name
 }

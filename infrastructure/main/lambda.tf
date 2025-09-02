6d1e17ac83d8a4acbdb666880ff1fd1ea6e36478
Jack Lavelle <your_email@example.com>
2025-09-02 14:57:13 +0100

    lambda

--- infrastructure/main/lambda.tf
+++ infrastructure/main/lambda.tf
@@ -1,61 +1,61 @@
-module "get_job_function" {
-  source = "../modules/lambda_container"
-  image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.get_job_repo_name}:latest"
-  function_name = "get_job"
-  policy_statements = [
-    {
-      sid       = "Logs"
-      effect    = "Allow"
-      actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-      resources = ["arn:aws:logs:*:*:*"]
-    },
-    {
-      sid       = "test"
-      effect    = "Allow"
-      actions   = ["*"]
-      resources = ["*"]
-    }
-  ]
-}
+# module "get_job_function" {
+#   source = "../modules/lambda_container"
+#   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.get_job_repo_name}:latest"
+#   function_name = "get_job"
+#   policy_statements = [
+#     {
+#       sid       = "Logs"
+#       effect    = "Allow"
+#       actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+#       resources = ["arn:aws:logs:*:*:*"]
+#     },
+#     {
+#       sid       = "test"
+#       effect    = "Allow"
+#       actions   = ["*"]
+#       resources = ["*"]
+#     }
+#   ]
+# }
 
 
 
-module "transcriber_function" {
-  source = "../modules/lambda_container"
-  image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.transcriber_repo_name}:latest"
-  function_name = "transcriber"
-    policy_statements = [
-    {
-      sid       = "Logs"
-      effect    = "Allow"
-      actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-      resources = ["arn:aws:logs:*:*:*"]
-    },
-    {
-      sid       = "test"
-      effect    = "Allow"
-      actions   = ["*"]
-      resources = ["*"]
-    }
-  ]
-}
+# module "transcriber_function" {
+#   source = "../modules/lambda_container"
+#   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.transcriber_repo_name}:latest"
+#   function_name = "transcriber"
+#     policy_statements = [
+#     {
+#       sid       = "Logs"
+#       effect    = "Allow"
+#       actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+#       resources = ["arn:aws:logs:*:*:*"]
+#     },
+#     {
+#       sid       = "test"
+#       effect    = "Allow"
+#       actions   = ["*"]
+#       resources = ["*"]
+#     }
+#   ]
+# }
 
-module "uploader_function" {
-  source = "../modules/lambda_container"
-  image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.uploader_repo_name}:latest"
-  function_name = "uploader"
-    policy_statements = [
-    {
-      sid       = "Logs"
-      effect    = "Allow"
-      actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-      resources = ["arn:aws:logs:*:*:*"]
-    },
-    {
-      sid       = "test"
-      effect    = "Allow"
-      actions   = ["*"]
-      resources = ["*"]
-    }
-  ]
-}
+# module "uploader_function" {
+#   source = "../modules/lambda_container"
+#   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.uploader_repo_name}:latest"
+#   function_name = "uploader"
+#     policy_statements = [
+#     {
+#       sid       = "Logs"
+#       effect    = "Allow"
+#       actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+#       resources = ["arn:aws:logs:*:*:*"]
+#     },
+#     {
+#       sid       = "test"
+#       effect    = "Allow"
+#       actions   = ["*"]
+#       resources = ["*"]
+#     }
+#   ]
+# }

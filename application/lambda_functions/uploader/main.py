534f3f6da040eb2851216cb3c997402e53ebbe01
Jack Lavelle <your_email@example.com>
2025-09-05 23:14:38 +0100

    lambda

--- application/lambda_functions/uploader/main.py
+++ application/lambda_functions/uploader/main.py
@@ -1,11 +1,20 @@
 import json
 
 def lambda_handler(event, context):
-    print(event)
+    body = json.loads(event["body"])
+    voice_type = body.get("voiceType")
+    file_name = body.get("fileName")
+
+    print(f"Voice type: {voice_type}")
+    print(f"file Name: {file_name}")
+
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps(event)
+        "body": json.dumps({
+            "recievedVoiceType": voice_type,
+            "recievedFileName": file_name
+        })
     }

e76dbbd960da54662d1d528e300464233d4088f3
Jack Lavelle <your_email@example.com>
2025-09-05 11:41:14 +0100

    lambda

--- infrastructure/main/lambda.tf
+++ infrastructure/main/lambda.tf
@@ -1,7 +1,7 @@
 module "get_job_function" {
   source           = "../modules/lambda_container"
   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.get_job_repo_name}:latest"
-  function_name    = "get_job"
+  function_name    = "${local.resource_name_prefix}-get_job"
   lambda_role_name = "${local.resource_name_prefix}-get-job-role"
   policy_statements = [
     {
@@ -24,7 +24,7 @@ module "get_job_function" {
 module "transcriber_function" {
   source           = "../modules/lambda_container"
   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.transcriber_repo_name}:latest"
-  function_name    = "transcriber"
+  function_name    = "${local.resource_name_prefix}-transcriber"
   lambda_role_name = "${local.resource_name_prefix}-transcriber-role"
 
   policy_statements = [
@@ -46,7 +46,7 @@ module "transcriber_function" {
 module "uploader_function" {
   source           = "../modules/lambda_container"
   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.uploader_repo_name}:latest"
-  function_name    = "uploader"
+  function_name    = "${local.resource_name_prefix}-uploader"
   lambda_role_name = "${local.resource_name_prefix}-uploader-role"
 
   policy_statements = [

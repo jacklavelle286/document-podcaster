89b35f430d0d2712a5ced3d9231351569b3d361f
Jack Lavelle <your_email@example.com>
2025-09-05 11:46:45 +0100

    lambda

--- application/lambda_functions/uploader/main.py
+++ application/lambda_functions/uploader/main.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dmps('Hello from uploader!')
+        'body': json.dumps('Hello from uploader!')
     }
     

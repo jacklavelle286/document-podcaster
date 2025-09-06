0a91e79b5b29c28e5c3012bca4f75910f703b875
Jack Lavelle <your_email@example.com>
2025-09-06 21:37:35 +0100

    adding test get-voices api

--- application/lambda_functions/get-voices/main.py
+++ application/lambda_functions/get-voices/main.py
@@ -3,9 +3,5 @@ import json
 def lambda_handler(event, context):
     voices = ["Normal", "Interesting", "Funky"]
     return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-        },
         "body": json.dumps({ "voices": voices })
     }

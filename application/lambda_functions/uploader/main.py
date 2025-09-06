938a8a7e269b9fd10c60297482e466972ce03462
Jack Lavelle <your_email@example.com>
2025-09-06 07:52:54 +0100

    lambda_presigned url

--- application/lambda_functions/uploader/main.py
+++ application/lambda_functions/uploader/main.py
@@ -1,4 +1,7 @@
 import json
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     body = json.loads(event["body"])
@@ -8,13 +11,29 @@ def lambda_handler(event, context):
     print(f"Voice type: {voice_type}")
     print(f"file Name: {file_name}")
 
+    url = createPresignedUrl(bucket="document-podcastor-poc-eu-west-2-upload", object_name="file", expiration=3600)
+
+
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "recievedVoiceType": voice_type,
-            "recievedFileName": file_name
+            "url": url
         })
     }
+
+
+def createPresignedUrl(bucket, object_name, expiration):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': bucket, 'Key': object_name},
+            ExpiresIn=expiration,
+        )
+    except ClientError as e:
+        logging.error(e)
+        return None
+    return response

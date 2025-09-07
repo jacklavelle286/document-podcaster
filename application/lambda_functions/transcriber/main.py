e8e3e036aeb94f30dccff093a2b97dc84df0670d
Jack Lavelle <your_email@example.com>
2025-09-07 19:01:45 +0100

    transcribe function

--- application/lambda_functions/transcriber/main.py
+++ application/lambda_functions/transcriber/main.py
@@ -14,8 +14,8 @@ def lambda_handler(event, context):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.info(event)
-
-    local_file_path = docx_to_text("JL-CV-AWS-Latest.docx")
+    file_name = event['Records'][0]['s3']['object']['key']
+    local_file_path = docx_to_text(file_name)
     with open(local_file_path, "r") as f:
         first_line = f.readline()
 
@@ -63,3 +63,10 @@ def transcriber(engine, language, output_format, s3_bucket, voiceId):
     return response
     
     
+
+data = {'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'eu-west-2', 'eventTime': '2025-09-07T17:54:18.417Z', 'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'AWS:AROAZOW6U4XNNEOXYQCC6:document-podcastor-poc-eu-west-2-uploader'}, 'requestParameters': {'sourceIPAddress': '81.159.120.59'}, 'responseElements': {'x-amz-request-id': '2P7M2DNQHNCSJA2A', 'x-amz-id-2': 'B8TdJy0jZzGjTgauoRAy6KF++ZU1oz8m2RBoAThx5vY/S2wwlDijBwXhYGxtKBB+uXwta3BPum246VmasMCxDaTEgZaSAD0s'}, 's3': {'s3SchemaVersion': '1.0', 'configurationId': 'tf-s3-lambda-20250906172940190100000001', 'bucket': {'name': 'document-podcastor-poc-eu-west-2-upload', 'ownerIdentity': {'principalId': 'A2U57DJ495TJCD'}, 'arn': 'arn:aws:s3:::document-podcastor-poc-eu-west-2-upload'}, 'object': {'key': 'JL-CV-AWS-Latest.docx', 'size': 18252, 'eTag': '02e7bf9f206c85a04b094ffbf86187f6', 'sequencer': '0068BDC6CA617DD210'}}}]}
+
+bucket_name = data['Records'][0]['s3']['bucket']['name']
+file_name = data['Records'][0]['s3']['object']['key']
+print(bucket_name)
+print(file_name)

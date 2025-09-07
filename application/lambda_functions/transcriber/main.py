d51e341e8a14d224a393f74d02283d51019c0edd
Jack Lavelle <your_email@example.com>
2025-09-07 14:56:01 +0100

    transcribe function

--- application/lambda_functions/transcriber/main.py
+++ application/lambda_functions/transcriber/main.py
@@ -6,13 +6,39 @@ from botocore.exceptions import ClientError
 
 
 def lambda_handler(event, context):
-    print(event)
+    '''
+    1. Get event from s3 upload
+    2. Trigger transcription job
+    3. Send output to an output s3 bucket
+    '''
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    output_bucket_name = os.environ["DESTINATION_BUCKET"]
+    logger.info(f"Got s3 bucket name: {output_bucket_name}")
+    response = transcriber(
+        engine="standard",
+        language="en-GB",
+        output_format="mp3",
+        s3_bucket=output_bucket_name,
+        output_prefix="mp3",
+        voiceId="Emmma"
+    )
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps(response)
     }
 
 
 
-def transcriber():
+def transcriber(engine, language, output_format, s3_bucket, output_prefix, voiceId):
+    polly = boto3.client("polly")
+    response = polly.synthesize_speech(
+            Engine=engine,
+            LanguageCode=language,
+            OutputFormat=output_format, 
+            OutputS3BucketName=s3_bucket,
+            OutputS3KeyPrefix=output_prefix,
+            VoiceId=voiceId
+    )
+    
     
--- infrastructure/main/lambda.tf
+++ infrastructure/main/lambda.tf
@@ -26,6 +26,10 @@ module "transcriber_function" {
   image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.transcriber_repo_name}:latest"
   function_name    = "${local.resource_name_prefix}-transcriber"
   lambda_role_name = "${local.resource_name_prefix}-transcriber-role"
+  environment_variables = {
+    DESTINATION_BUCKET = module.outputs_bucket.bucket_name
+  }
+
 
 
   policy_statements = [
@@ -45,10 +49,10 @@ module "transcriber_function" {
 }
 
 module "uploader_function" {
-  source           = "../modules/lambda_container"
-  image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.uploader_repo_name}:latest"
-  function_name    = "${local.resource_name_prefix}-uploader"
-  lambda_role_name = "${local.resource_name_prefix}-uploader-role"
+  source                = "../modules/lambda_container"
+  image_uri             = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.uploader_repo_name}:latest"
+  function_name         = "${local.resource_name_prefix}-uploader"
+  lambda_role_name      = "${local.resource_name_prefix}-uploader-role"
   log_retention_in_days = 3
   environment_variables = {
     UPLOAD_BUCKET = module.upload_bucket.bucket_name
@@ -72,10 +76,10 @@ module "uploader_function" {
 
 
 module "get_voices_function" {
-  source           = "../modules/lambda_container"
-  image_uri        = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.get_voices_repo_name}:latest"
-  function_name    = "${local.resource_name_prefix}-get-voices"
-  lambda_role_name = "${local.resource_name_prefix}-get-voices-role"
+  source                = "../modules/lambda_container"
+  image_uri             = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${local.get_voices_repo_name}:latest"
+  function_name         = "${local.resource_name_prefix}-get-voices"
+  lambda_role_name      = "${local.resource_name_prefix}-get-voices-role"
   log_retention_in_days = 3
 
   policy_statements = [
